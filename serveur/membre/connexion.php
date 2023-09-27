e709083823f8f873acd68167e95d686f23c22d01
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2023-09-27 16:04:37 -0400

    modelMembre->enregistrerMembre & modelConnexion ->connexion

---
+++ serveur/membre/connexion.php
@@ -0,0 +1,30 @@
+<?php
+    require_once(__DIR__.'/../bd/connexion.inc.php');
+
+    function Mdl_Connexion($courriel, $mdp){
+
+        try{
+            $requete = "SELECT * FROM connexion WHERE courriel=? AND motdepasse=?";
+            $stmt = $connexion->prepare($requete);
+            $stnt->bind_param("ss", $courriel, $mdp);
+            $stmt ->execute();
+            $reponse-> $stmt->get_result();
+            if ($reponse->num_rows > 0) {
+                $ligne = $reponse->fetch_object();
+                if($ligne->role == 'M'){
+                    $_SESSION['role'] = 'M';
+                    header('Location: ../membre/membre.php');//changer pour membre quand implémenter
+                    exit();
+                } else {
+                    $_SESSION['role'] = 'A';
+                    header('Location: ../admin/admin.php');//changer pour admin quand implémenter
+                    exit();
+                }
+            }
+            else{
+                header("Location: ../../index.php?msg=$msg");//changer pour connexion quand implémenter
+            exit;
+            }
+        }
+    }
+?>
