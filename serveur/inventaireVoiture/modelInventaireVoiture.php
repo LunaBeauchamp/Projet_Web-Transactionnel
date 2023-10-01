cfaf8ac7aefdf73e52f0a004bcf5534e548d5524
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2023-10-01 17:13:20 -0400

    get inventaire

---
+++ serveur/inventaireVoiture/modelInventaireVoiture.php
@@ -0,0 +1,17 @@
+<?php
+    require_once(__DIR__.'/../bd/connexion.inc.php');
+
+    function Mdl_Lister(){
+        global $connexion;
+        try{
+            $requete = "SELECT * FROM inventaireVoiture";
+            $stmt = $connexion->prepare($requete);
+            $stmt->execute();
+            $reponse = $stmt->get_result();
+        } catch(Exception $e) {
+            return [];
+        }finally{
+            return $reponse;
+        }
+    }
+?>
