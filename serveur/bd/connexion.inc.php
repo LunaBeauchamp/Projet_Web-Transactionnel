27653ad758c0c0761728cd8cdd482c4580966735
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2023-10-16 14:26:25 -0400

    lister voiture Acceuil

--- serveur/bd/connexion.inc.php
+++ serveur/bd/connexion.inc.php
@@ -1,8 +1,38 @@
 <?php
-    require_once(__DIR__.'/../env/env.inc.php');
-    $connexion = new mysqli(SERVEUR,USAGER,MDP,BD,PORT);
-    if($connexion->connect_errno) {
-        echo "Probléme de connexion au serveur de bd";
-        exit();
-    }
+declare (strict_types=1);
+
+require_once(__DIR__.'/../env/env.inc.php');
+
+// Patron de conception Singleton
+class Connexion{
+	private static $connexion=null;
+	
+	// Interdire de créer des objets Connexion par l'extérieur de la classe
+	private function __construct(){}
+
+	
+	// Retourne le singleton de la connexion
+	static function getConnexion():PDO {
+		if(self::$connexion == null){
+			self::connecter();
+		}
+		return self::$connexion;
+	}
+	
+	// Créer la connexion
+	private static function connecter():void {
+		global $SERVEUR, $BD, $USAGER, $PASS; // Définies dans le fichier "env.inc.php"
+		try {
+			$dns = "mysql:host=$SERVEUR;dbname=$BD";
+			$options = array(
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+			);
+			self::$connexion = new PDO( $dns, $USAGER, $PASS, $options );
+			} catch ( Exception $e ) {
+				echo $e->getMessage();
+				echo "Probleme de connexion au serveur de bd";
+				exit();
+			}
+	}
+}
 ?>
--- serveur/env/env.inc.php
+++ serveur/env/env.inc.php
@@ -1,7 +1,6 @@
 <?php
-    define('SERVEUR','127.0.0.1');
-    define('USAGER','root');
-    define('MDP','');
-    define('BD','bdboutique');
-    define('PORT',3306)
+	$SERVEUR = "127.0.0.1";
+	$USAGER = "root";
+	$PASS = "";
+	$BD = "bdboutique";
 ?>
