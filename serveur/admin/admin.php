264c0494f74b2d18ed24b6d15e3b8fb6062258cc
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2023-11-09 09:48:14 -0500

    cliquer sur acceuil charge tout

--- serveur/admin/admin.php
+++ serveur/admin/admin.php
@@ -40,7 +40,7 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="javascript:listerPar('id')">Accueil</a>
+						<a class="nav-link active" aria-current="page" href="javascript:chargerVoituresAJAX('table','../../routes.php');">Accueil</a>
 					</li>
 
 					<li class="nav-item dropdown">
